1eb3b218ac569bebd1a142372c652ad7b29abc76
Wasin  Mekkit <Wasinmekkit@gmail.com>
2020-11-26 15:34:01 +0700

    create navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <!--  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <b-navbar class="is-light main__navbar">
-      <template slot="brand">
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <img src="@/assets/logo.png" alt="Vuejs logo" />
-        </b-navbar-item>
-      </template>
-      <template slot="start">
-        <b-navbar-item tag="router-link" :to="{ path: '/' }"> Home </b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/about' }"> About </b-navbar-item>
-      </template>
-    </b-navbar>
+    <MainHeader />
     <router-view />
+    <MainFooter />
   </div>
 </template>
+
+<script>
+import MainHeader from "@/components/MainHeader.vue";
+import MainFooter from "@/components/MainFooter.vue";
+export default {
+  components: {
+    MainHeader,
+    MainFooter,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap");
@@ -26,13 +24,5 @@
   font-weight: 100;
   text-align: center;
   color: #2c3e50;
-}
-.main__navbar {
-  a {
-    font-weight: 600;
-  }
-  a:hover {
-    background: #bcbcbc;
-  }
 }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar class="is-light main__navbar">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img src="@/assets/logo.png" alt="Vuejs logo" />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }"> Home </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/about' }"> About </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+};
+</script>
+
+<style lang="scss">
+.main__navbar {
+  a {
+    font-weight: 600;
+  }
+  a:hover {
+    background: #bcbcbc;
+  }
+}
+</style>
