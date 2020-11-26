cefa14673ecd4b297195a196927f1ae0e3fb606e
Wasin  Mekkit <Wasinmekkit@gmail.com>
2020-11-26 14:00:55 +0700

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!--  <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+    <b-navbar class="is-light main__navbar">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="@/assets/logo.png" alt="Vuejs logo" />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }"> Home </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/about' }"> About </b-navbar-item>
+      </template>
+    </b-navbar>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Roboto Condensed", sans-serif;
+  font-weight: 100;
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
+.main__navbar {
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    font-weight: 600;
+  }
+  a:hover {
+    background: #bcbcbc;
   }
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page ที่ 2 นี่คือ ข้อความทดสอบ</h1>
   </div>
 </template>
